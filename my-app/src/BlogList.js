fbc74f0b535ac25d579ac2ce72754dfa85e252f4
julia-yao <119920098+julia-yao@users.noreply.github.com>
2023-07-01 00:16:41 +0800

    fetching data

--- my-app/src/BlogList.js
+++ my-app/src/BlogList.js
@@ -1,19 +1,15 @@
-const BlogList = ({blogs,title, handleDelete}) => {
-    //const blogs = props.blogs;
-    //const title = props.title;
-    return ( 
-        
-        <div className="blog-list">
-            <h2>{ title }</h2>
-            {blogs.map((blog)=>(
-               <div className="blog-preview" key={blog.id}>
-                   <h2>{blog.title}</h2>
-                   <p>written by {blog.author} .</p>
-                   <button onClick={() =>handleDelete(blog.id)}>delete</button>
-               </div>
-           ))}
-        </div>
-     );
-}
- 
-export default BlogList;
+const BlogList = ({ blogs }) => {
+    return (
+      <div className="blog-list">
+        {blogs.map(blog => (
+          <div className="blog-preview" key={blog.id} >
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+            
+          </div>
+        ))}
+      </div>
+    );
+  }
+   
+  export default BlogList;
--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,33 +1,24 @@
-import{ useEffect, useState } from 'react';
-import BlogList from './BlogList';
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body:'loredmmv mlmoe dmo...', author:'mario',id:1},
-        {title: 'Welcome party', body:'loredmmv mlmoe dmo...', author:'yoshi',id:2},
-        {title: 'Web dev top tips', body:'loredmmv mlmoe dmo...', author:'wolf',id:3},
-    ]);
-    const [name,setName] = useState('zaozao');
+  const [blogs, setBlogs] = useState(null)
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog =>blog.id !== id );
-        setBlogs(newBlogs); 
-    }
-    //刪除物件觸發更新狀態 重新
-    useEffect(()=> {
-        console.log('use effect ran');
-        console.log(name);
-    },[name])
-    //可以作為參數來觸發
-     
-    return (   
-        <div className="Home">
-           <BlogList blogs={blogs} title = 'All blogs!' handleDelete={handleDelete} />
-           <button onClick={()=>setName('Liliy')}>change name</button>
-           <p>{ name }</p>
-        </div>
-     );
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      });
+  }, [])
+
+  return (
+    <div className="home">
+      {blogs && <BlogList blogs={blogs} />}
+    </div>
+  );
 }
  
 export default Home;
