8871181608055e7848ed552aa342ea797be622fd
julia-yao <119920098+julia-yao@users.noreply.github.com>
2023-07-06 18:10:42 +0800

    class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
-
-import Navbar from './Navbar';
-import Home from './Home';
-
-
-function App(props) {
-  return (
-    <button>{props.children}</button>
-  );
-}
-
+import React, { Component } from 'react';
+class App extends Component{
+    constructor(props) { // 加入建構子以及props參數
+        super(props);
+        this.name="哈哈";
+        this.changeName=this.changeName.bind(this);
+    };
+    changeName(newName){ // 定義changeName
+      this.name=newName;
+      console.log("hey")
+    };
+    render(){
+      return(
+        <button onClick={this.changeName}>{this.name} </button>
+      );
+  }
+  }
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,15 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
- 
-const printMessage=()=>{
-  document.getElementById('show-area').innerHTML="我被按到了";
-}
 
 root.render(
   <div>
-      <App> 在index.js中設定文字 </App>
-  </div>,
+        <App/>
+    </div>,
 );
 
 // If you want to start measuring performance in your app, pass a function
