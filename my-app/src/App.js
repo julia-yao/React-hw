625a15308afbfc629e4fe8e8a0c88272c968df25
julia-yao <119920098+julia-yao@users.noreply.github.com>
2023-07-06 18:32:29 +0800

    setState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 class App extends Component{
-    constructor(props) { // 加入建構子以及props參數
-        super(props);
-        this.name="哈哈";
-        this.changeName=this.changeName.bind(this);
-    };
-    changeName(newName){ // 定義changeName
-      this.name=newName;
-      console.log("hey")
-    };
+  constructor(props) {
+    super(props);
+    this.state={
+      percent:"30%"
+    }
+    this.changePercent=this.changePercent.bind(this); //綁定changePercent
+    
+  }
+  //加入函式
+  changePercent(){ 
+    this.setState({percent:"70%"})
+  }
+  
+
+
     render(){
-      return(
-        <button onClick={this.changeName}>{this.name} </button>
-      );
-  }
-  }
+        return(
+          <div>
+            <div className="progress-back" style={{backgroundColor:"rgba(0,0,0,0.2)",width:"200px",height:"7px",borderRadius:"10px"}}>
+              <div className="progress-bar" style={{backgroundColor:"#fe5196",width:this.state.percent,height:"100%",borderRadius:"10px"}}></div>
+            </div>
+            <button onClick={this.changePercent}>轉換百分比 </button>
+          </div>
+        );
+    }
+}
 export default App;
