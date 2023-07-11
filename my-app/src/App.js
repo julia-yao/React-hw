2049308d4c19141b90df4d335cec03a409610cda
julia-yao <119920098+julia-yao@users.noreply.github.com>
2023-07-11 13:28:51 +0800

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,12 @@
-
 import Navbar from './Navbar';
 import Home from './Home';
 
-
 function App() {
   return (
-    <div className="App"> 
-      <Navbar/>
-      <div className='content'>
-        <Home/>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
       </div>
     </div>
   );
--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -2,21 +2,26 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
+
+  const [name, setName] = useState('mario');
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-      });
-  }, [])
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      setBlogs(data);
 
+    })
+  }, [name])
+   //blogs && 邏輯判斷 有條件的輸出模板
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} />}
+      { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
+      
     </div>
   );
 }
