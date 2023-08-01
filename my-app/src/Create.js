24bea63ddae2f108cf13b9ebebb4cb1d28c59217
julia-yao <119920098+julia-yao@users.noreply.github.com>
2023-08-01 19:47:48 +0800

    Controlled Inputs

--- my-app/src/Create.js
+++ my-app/src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('max');
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text" 
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="max">max</option>
+                    <option value="tsuda">tsuda</option>
+                </select>
+                <button>Add blog</button>
+                <p>{ title }</p>
+                <p>{ body }</p>
+                <p>{ author }</p>
+            </form>
         </div>
      );
 }
